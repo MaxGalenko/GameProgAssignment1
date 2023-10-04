3527cdc58bcd437f22ebc4413f13b5f6e85f070f
Maxym Galenko <2060352@edu.vaniercollege.qc.ca>
2023-10-04 10:17:15 -0400

    Animations still don't work

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,10 +9,10 @@ public class CharacterMovement : MonoBehaviour
     Vector3 move;
 
     public float walkSpeed = 5;
-    public float runSpeed = 8; 
+    public float runSpeed = 8;
     public float jumpHeight = 2;
     public float gravity = -9.18f;
-    
+
     private CharacterController controller;
     private Animator animator;
 
@@ -33,7 +33,7 @@ public class CharacterMovement : MonoBehaviour
 
     public void LateUpdate()
     {
-       UpdateAnimator();
+        UpdateAnimator();
     }
 
     void DisableRootMotion()
@@ -46,9 +46,9 @@ public class CharacterMovement : MonoBehaviour
         bool isGrounded = controller.isGrounded;
         // TODO 
         Debug.Log(isGrounded);
-        if(move != Vector3.zero)
+        if (move != Vector3.zero)
         {
-            if(GetMovementSpeed() == runSpeed)
+            if (GetMovementSpeed() == runSpeed)
             {
                 animator.SetFloat("Speed", 1.0f);
             }
@@ -63,10 +63,16 @@ public class CharacterMovement : MonoBehaviour
         }
 
         animator.SetBool("isGrounded", isGrounded);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.applyRootMotion = true;
+            animator.SetTrigger("doRoll");
+        }
     }
 
     void ProcessMovement()
-    { 
+    {
         move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if (move != Vector3.zero)
         {
@@ -78,26 +84,26 @@ public class CharacterMovement : MonoBehaviour
     {
         bool isGrounded = controller.isGrounded;
         // Since there is no physics applied on character controller we have this applies to reapply gravity
-        
-        if (isGrounded  )
+
+        if (isGrounded)
         {
             if (playerVelocity.y < 0.0f) // we want to make sure the players stays grounded when on the ground
             {
                 playerVelocity.y = -1.0f;
             }
-            
+
             if (Input.GetButtonDown("Jump")) // Code to jump
             {
-                playerVelocity.y +=  Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
             }
         }
         else // if not grounded
         {
             playerVelocity.y += gravity * Time.deltaTime;
-        }       
+        }
 
         controller.Move(move * Time.deltaTime * GetMovementSpeed() + playerVelocity * Time.deltaTime);
-        
+
     }
 
     float GetMovementSpeed()
